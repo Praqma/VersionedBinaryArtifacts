b010101a8e7d11af25f4b5254065c8a369fcaee3
naesheim <hg.nesheim@gmail.com>
2017-02-17 10:24:59 +0100

    add Oauth push

--- .travis/post_build_success.sh
+++ .travis/post_build_success.sh
@@ -3,4 +3,4 @@ set -e
 git fetch origin master:refs/remotes/origin/master \
 && git checkout -b master origin/master \
 && git merge develop \
-&& git push "https://${GITHUB_SECRET_TOKEN}@github.com/Praqma/VersionedBinaryArtifacts.git" -u origin master
+&& git push "https://${GITHUB_SECRET_TOKEN}@github.com/Praqma/VersionedBinaryArtifacts.git" master
