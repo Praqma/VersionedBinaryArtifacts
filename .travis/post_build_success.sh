25fba134e4abecbb9d3b559e1dcdd991e613f5d7
naesheim <hg.nesheim@gmail.com>
2017-02-17 10:54:48 +0100

    hide log from secret token

--- .travis/post_build_success.sh
+++ .travis/post_build_success.sh
@@ -3,4 +3,4 @@ set -e
 git fetch origin master:refs/remotes/origin/master \
 && git checkout -b master origin/master \
 && git merge develop \
-&& git push "https://${GITHUB_SECRET_TOKEN}@github.com/Praqma/VersionedBinaryArtifacts.git" master
+&& git push "https://${GITHUB_SECRET_TOKEN}@github.com/Praqma/VersionedBinaryArtifacts.git" master >/dev/null 2>&1
